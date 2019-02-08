2820195bbe7c19ff4aa2927d78c8ddcd186a0074
Nattawod Satee <nattawodsatee@gmail.com>
2019-02-08 11:44:24 -0800

    Setting up auth

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+const keys = require('../config/keys');
 
-const uri = "mongodb://golfzoozoo:test1234@ds123645.mlab.com:23645/auth-myself";
+const uri = `mongodb://${keys.username}:${keys.password}@ds123645.mlab.com:23645/auth-myself`;
 
-mongoose.connect(uri, { useNewUrlParser: true }).then(
+mongoose.connect(uri, { useNewUrlParser: true }).then( 
     () => {
         console.log("connected to db");
     },
